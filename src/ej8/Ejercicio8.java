2e579b1b9507671a804c4d0adc16bce2ea0ce37b
af3d3z <alonso.fernandez@iesnervion.es>
2024-10-03 10:09:49 +0200

    Ejercicios acabados

--- src/ej8/Ejercicio8.java
+++ src/ej8/Ejercicio8.java
@@ -1,5 +1,19 @@
 package ej8;
 
 public class Ejercicio8 {
-	
+	public static int fibonacci(int n) throws Exception {
+		int res;
+		int n1 = n - 1;
+		int n2 = n - 2;
+		
+		if (n == 1 || n == 2) {
+			res = 1;
+		}else if(n < 1) {
+			throw new Exception("No puede ser menor a 1.");
+		}else {
+			return fibonacci(n1) + fibonacci(n2);
+		}
+		
+		return res;
+	}
 }
--- src/ej8/Ejercicio8Test.java
+++ src/ej8/Ejercicio8Test.java
@@ -1,14 +1,37 @@
 package ej8;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class Ejercicio8Test {
 
+	@ParameterizedTest
+	@MethodSource("numeritos")
+	void testFibonacci(int n, int resEsperado) throws Exception {
+		int res = Ejercicio8.fibonacci(n);
+		assertEquals(resEsperado, res);
+	}
+	
+	
+	public static Stream<Arguments> numeritos() {
+		return Stream.of(
+					Arguments.of(1, 1),
+					Arguments.of(2, 1),
+					Arguments.of(3, 2),
+					Arguments.of(5, 5)
+				);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testFibonacciException() throws Exception {
+		assertThrows(Exception.class, () -> Ejercicio8.fibonacci(0), "No puede ser menor a 1.");
 	}
 
 }
